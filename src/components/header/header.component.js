875e03957d2d0431b9db0d8f7d7f981ac197890a
Christian Montero Martínez <christianmontero08@gmail.com>
2020-07-07 21:53:24 -0700

    Added cart-icon and cart-dropdown component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -8,7 +8,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/original.svg';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return(
         <div className="header">
             <Link className="logo-container" to="/">
@@ -26,13 +26,16 @@ const Header = ({ currentUser }) => {
                 }
                 <CartIcon/>
             </div>
-            <CartDropdown/>
+            {
+                hidden ? null : <CartDropdown/> 
+            }
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser,
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
