8ea15d5b1c68a497ae11dcefc4d6899bf69cf41a
Christian Montero Martínez <christianmontero08@gmail.com>
2020-07-08 21:57:29 -0700

    Updated shop data with new data and updated cart utils to group items in cart reducer

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -4,27 +4,23 @@ import CollectionPreview from '../../components/collection-preview/collection-pr
 
 class ShopPage extends React.Component{
     
-    constructor(props){
+    constructor(props) {
         super(props);
-
+    
         this.state = {
             collections: SHOP_DATA
-        }
+        };
     }
 
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => {
-                        return(
-                            <CollectionPreview key={id} {...otherCollectionProps}/>
-                        )
-                    })
-                }
-            </div>
-        )
+    render() {
+        const { collections } = this.state;
+        return (
+          <div className='shop-page'>
+            {collections.map(({ id, ...otherCollectionProps }) => (
+              <CollectionPreview key={id} {...otherCollectionProps} />
+            ))}
+          </div>
+        );
     }
 
 }
